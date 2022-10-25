21fcf5c7b229798baf7567e07fa513498d9be9f9
neevek <i@neevek.net>
2022-10-25 22:03:26 +0800

    test

--- dev.sh
+++ dev.sh
@@ -6,5 +6,5 @@ IFS=$'\n\t'
  # bash -c 'cd frontend; CARGO_TARGET_DIR=../target-trunk trunk serve --address 0.0.0.0' & \
  # bash -c 'cd backend; cargo watch -- cargo run -- --port 8081')
 
- bash -c 'cd frontend; trunk serve --address 0.0.0.0 --port 8082 --proxy-backend=http://[::1]:8081/api' & \
+ bash -c 'cd frontend; trunk serve --address 0.0.0.0 --port 8082 --proxy-backend=http://0.0.0.0:8081/api' & \
  bash -c 'cargo watch -w backend/src -- cargo run --bin backend -- --port 8081 --serve-dir ./testdir --assets-dir ./frontend/dist')
